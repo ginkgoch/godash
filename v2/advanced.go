7602b608274767771d2b0e79c9ecb455533555ef
howardch <howardch@outlook.com>
2022-01-11 22:11:15 +0800

    add dashslice

--- v2/advanced.go
+++ v2/advanced.go
@@ -7,3 +7,38 @@ type Number interface {
 type DashAny interface {
 	Number | string | interface{}
 }
+
+type DashSlice[E any] []E
+
+func Map1[E, V any](slice *DashSlice[E], iteratee Iteratee[E, V]) *DashSlice[V] {
+	result := DashSlice[V]{}
+	for _, item := range *slice {
+		result = append(result, iteratee(item))
+	}
+
+	return &result
+}
+
+func Map[E, V any](slice *[]E, iteratee func(E) V) *[]V {
+	result := []V{}
+	for _, item := range *slice {
+		result = append(result, iteratee(item))
+	}
+
+	return &result
+}
+
+func (slice *DashSlice[E]) Filter(predicate Predicate[E]) *DashSlice[E] {
+	result := DashSlice[E]{}
+	for _, item := range *slice {
+		if predicate(item) {
+			result = append(result, item)
+		}
+	}
+
+	return &result
+}
+
+type Iteratee[E any, V any] func(E) V
+
+type Predicate[E any] func(E) bool
